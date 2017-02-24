f256462c56d6c1af93b48a1ec70c8b21dca06d3b
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2017-02-24 12:49:28 -0430

    Formulario de participación incompleto pero funcional
    Siguientes pasos:
    - Completar formulario para ingresar a las mascotas.
    - Formulario de registro y login para los administradores.
    - Subir las imágenes a la base de datos.
    - Validar una única participación.

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -1,5 +1,5 @@
 // Inicializando Firebase
-var config = 
+const config = 
 {
 	apiKey: "AIzaSyC9ZFYpocgv1cuYzyBx768ewsYgqs12g2g",
     authDomain: "mimascota2017-ff3b5.firebaseapp.com",
@@ -7,7 +7,34 @@ var config =
     storageBucket: "mimascota2017-ff3b5.appspot.com",
     messagingSenderId: "252069980494"
 };
-
+console.log("saaaaaaaaaaaas");
 firebase.initializeApp(config);
-var db = firebase.database();
-var auth = firebase.auth();
+const db = firebase.database();
+const auth = firebase.auth();
+
+//Obteniendo todos los valores del formulario de participación
+const nomUsuario = document.getElementById("nombreUsuario");
+const apeUsuario = document.getElementById("apellidoUsuario");
+const teleUsuario = document.getElementById("telefonoUsuario");
+const cedUsuario = document.getElementById("cedulaUsuario");
+const enviarDatos = document.getElementById("botonSubmitForm");
+console.log("leí datos");
+
+//Mandando toda la información del formulario
+enviarDatos.addEventListener("click", function()
+{
+	//Obteniendo el valor de los inputs en el formulario de participación
+	const userName = nomUsuario.value;
+	const userApellido = apeUsuario.value;
+	const userTelf = teleUsuario.value;
+	const userid = cedUsuario.value;
+	console.log("entré");
+
+	db.ref('/formulario/participante/'+userid).set(
+	{
+		nombreParticipante: userName,
+		apellidoParticipante: userApellido,
+		telefonoParticipante: userTelf,
+		cedulaParticipante:	userid
+	});
+});
