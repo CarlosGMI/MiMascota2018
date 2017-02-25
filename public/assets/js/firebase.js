5fd48210c33bb63bb3f45f4a2f2728d9d7a43209
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2017-02-25 01:18:39 -0430

    Arreglado problema al llenar formulario y login funcional.
    Incluye también validación de solo subir 3 fotos (sin probar)

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -21,7 +21,6 @@ const nomMascota = document.getElementById("nombreMascota");
 const edMascota = document.getElementById("edadMascota");
 const histMascota = document.getElementById("historiaMascota");
 const enviarDatos = document.getElementById("botonSubmitForm");
-console.log("leí datos");
 
 //Obteniendo datos para El iniciar Sesion.................
 const textoEmail = document.getElementById('textoEmail');
@@ -32,107 +31,158 @@ const botonLogout = document.getElementById('botonLogout');
 const IniciarS = document.getElementById('IniciarS');
 const Perfil = document.getElementById('Perfil');
 // Obtener Elementos Para subir Archivos.............
-      var uploader = document.getElementById('uploader');
-      var fileButton = document.getElementById('fileButton');
+var uploader = document.getElementById('uploader');
+var fileButton = document.getElementById('fileButton');
+var cont = 0;
+console.log("leí datos");
 
+if(fileButton)
+{
+	fileButton.addEventListener('change', function(e) 
+	{
+	    if(!cedUsuario)
+	    {
+	    	window.alert("Debes insertar una cédula");
+	    }
+	    else
+	    {
+	    	cont = cont++;
+		    if(cont<=3)
+		    {	
+		    	console.log("Obteniendo el archivo");
+		    	//Obtener archivo
+			    var file = e.target.files[0];
+			    var storageRef = firebase.storage().ref('fotosMascota/'+idParticipante+'/'+file.name);
+			    var task = storageRef.put(file);
+			    //Actualizar barra progreso
+			    task.on('state_changed', function progress(snapshot) 
+			    {
+			        var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			        uploader.value = percentage;
+			    },
+			    function error(err){
+			    },
+			    function complete(){
+			    });
+			}
+			else
+			{
+				window.alert("Solo puedes subir 3 imágenes");
+			}
+    	}
+	});
+}
 
+//Sign in
+if(botonLogin)
+{
+	botonLogin.addEventListener('click', function()
+	{
+	    const email = textoEmail.value;
+	    const pass = textoPassword.value;
+	    auth.signInWithEmailAndPassword(email, pass).catch(function(error)
+	    {
+	    	var errorCode = error.code;
+	    	var errorMessage = error.message;
+	    	if(errorCode == 'auth/wrong-password')
+	    	{
+	    		window.alert("Contraseña equivocada! Vuelve a ingresar.");
+	    	}
+	    	else if(errorCode == 'auth/user-not-found')
+	    	{
+	    		window.alert("Tu correo electrónico no se encuentra registrado.");
+	    	}
+	    	else
+	    	{
+	    		window.alert("Error de autenticación");
+	    	}
+	    }).then(function()
+	    {
+	    	console.log("Me estoy logeando");
+	    	window.location.replace("index.html");
+	    });	
+	});
+}
 
-        // Aki Ocurre la Seleccion del archivo................
-      fileButton.addEventListener('change', function(e) {
-        //Obtener archivo
-        var file = e.target.files[0];
-        // Crear un storage ref
-        var storageRef = firebase.storage().ref('mis_fotos/' + file.name);
-         // Subir archivo
-        var task = storageRef.put(file);
-         // Actualizar barra progreso
-        task.on('state_changed',
-          function progress(snapshot) {
-            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            uploader.value = percentage;
-          },
-           function error(err) {
-          },
-          function complete() {
-          }
-          )
-      });
+//Sign Up
+if(botonSignUp)
+{
+	botonSignUp.addEventListener('click', function() 
+	{
+		const email = textoEmail.value;
+	    const pass = textoPassword.value;
+	    auth.createUserWithEmailAndPassword(email, pass).then(function()
+	    {
+	    	console.log("Me estoy registrando");
+	    	window.location.replace("index.html");
+	    });
+	});
+}
 
-//hata aki se selecciona las imageness..........................................
+//Logout
+if(botonLogout)
+{
+	botonLogout.addEventListener('click', function() 
+	{
+	    auth().signOut();
+	    console.log("Cerré sesión");
+	    window.location.replace("index.html");
+	});
+}
 
-  				// añadiendo enventos para el  login
-  				botonLogin.addEventListener('click', e => {
-    			//guardando mail y pass..........
-    				const email = textoEmail.value;
-    				const pass = textoPassword.value;
-    				const auth = firebase.auth();
-
-    				// Sign in
-    				const aux = auth.signInWithEmailAndPassword(email, pass);
-    				aux.catch(e => console.log(e.message));   
-  				});
-
-
- 				// añadiendo enventos para el signup...........
-  				botonSignUp.addEventListener('click', e => {
-    			// guardando email y pass.............
-    			
-    			const email = textoEmail.value;
-    			const pass = textoPassword.value;
-   				const auth = firebase.auth();
-    			// Sign in........................
-    			const aux2 = auth.createUserWithEmailAndPassword(email, pass);
-    			aux2.catch(e => console.log(e.message));
-  				});
-
-
-  				botonLogout.addEventListener('click', e => {
-    			firebase.auth().signOut();
-  				});
-
-  				 // añadiendo enventos  en tiempo real
-   				firebase.auth().onAuthStateChanged( firebaseUser => {
-    			if(firebaseUser) {
-      			console.log(firebaseUser);
-      			botonLogout.classList.remove('hide');
-      			botonSignUp.classList.remove('hide');
-      			IniciarS.classList.add('hide');
-      			Perfil.classList.remove('hide');
-    			}
-    			else
-    			{
-      			console.log('no logueado');
-      			botonLogout.classList.add('hide');
-      			botonSignUp.classList.add('hide');
-      			IniciarS.classList.remove('hide');
-        		Perfil.classList.add('hide');
-    			}    
-  				});
+//Eventos en tiempo real
+//auth().onAuthStateChanged(function(user)
+//{
+//	if(user)
+//	{
+//		console.log("Admin logeado");
+		/*console.log(firebaseUser);
+      	botonLogout.classList.remove('hide');
+      	botonSignUp.classList.remove('hide');
+      	IniciarS.classList.add('hide');
+      	Perfil.classList.remove('hide');*/
+//	}
+//	else
+//	{
+//		console.log("Admin no logeado")
+		/*console.log('no logueado');
+      	botonLogout.classList.add('hide');
+      	botonSignUp.classList.add('hide');
+      	IniciarS.classList.remove('hide');
+        Perfil.classList.add('hide');*/
+//	}
+//});  */
 
 //Mandando toda la información del formulario
-enviarDatos.addEventListener("click", function()
+if(enviarDatos)
 {
-	//Obteniendo el valor de los inputs en el formulario de participación
-	const userName = nomUsuario.value;
-	const userApellido = apeUsuario.value;
-	const userTelf = teleUsuario.value;
-	const userid = cedUsuario.value;
-	const petName = nomMascota.value;
-	const petAge = edMascota.value;
-	const petTale = histMascota.value;
-	console.log("entré");
+	enviarDatos.addEventListener("click", function()
+	{
+		//Obteniendo el valor de los inputs en el formulario de participación
+		const userName = nomUsuario.value;
+		const userApellido = apeUsuario.value;
+		const userTelf = teleUsuario.value;
+		const userid = cedUsuario.value;
+		const petName = nomMascota.value;
+		const petAge = edMascota.value;
+		const petTale = histMascota.value;
+		console.log("entré");
 
-	db.ref('/formulario/participante/'+userid).set(
-	{
-		nombreParticipante: userName,
-		apellidoParticipante: userApellido,
-		telefonoParticipante: userTelf,
-		cedulaParticipante:	userid
+		db.ref('/formulario/participante/'+userid).set(
+		{
+			nombreParticipante: userName,
+			apellidoParticipante: userApellido,
+			telefonoParticipante: userTelf,
+			cedulaParticipante:	userid
+		});
+		db.ref('/formulario/mascota/'+userid).set(
+		{
+			nombreMascota: petName,
+			edadMascota: petAge,
+			historiaMascota: petTale
+		}).then(function()
+		{
+			window.location.replace("participando.html");
+		});
 	});
-	db.ref('/formulario/mascota/'+userid).set(
-	{
-		nombreMascota: petName,
-		edadMascota: petAge,
-		historiaMascota: petTale
-	});
-});
+}
